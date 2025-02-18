7b164ac7f037488dcdc38839708850a6ab9677bb
rizkyayahyaa <rizkyayahya13@gmail.com>
2025-02-18 15:24:23 +0700

    building app

---
+++ database/migrations/2025_02_18_080838_create_items_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('items', function (Blueprint $table) {
+            $table->id();
+            $table->string('name'); // Nama item
+            $table->boolean('is_done')->default(false); // Status selesai/tidak
+            $table->foreignId('checklist_id')->constrained()->onDelete('cascade'); // Relasi ke checklist
+            $table->timestamps();
+        });
+    }
+
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('items');
+    }
+};
